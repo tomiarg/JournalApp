98829a2a6c0c642b6fe96967f59870712f627c77
Tomas Moyano <tomimoyanook@gmail.com>
2023-12-21 16:13:45 -0300

    including redux toolkit

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,15 +1,29 @@
-import { Typography } from "@mui/material"
+import { IconButton, Typography } from "@mui/material"
 import { JournalLayout } from "../layout/JournalLayout"
-import { NothingSelectedView } from "../views"
+import { NoteView, NothingSelectedView } from "../views"
+import { AddOutlined } from "@mui/icons-material"
 
 
 export const JournalPage = () => {
   return (
     <JournalLayout>
-      {/* <Typography>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum voluptatibus facere, suscipit ab, libero incidunt earum ex aliquid esse quis dolores porro cum. Aspernatur itaque cumque adipisci doloremque. Magni, eligendi.
-      </Typography> */}
+     
+      {/* <NoteView/> */}
       <NothingSelectedView/>
+
+      <IconButton
+        size="large"
+        sx={{
+          color:'white',
+          backgroundColor: 'error.main',
+          ':hover':{backgroundColor: 'error.main', opacity:0.9},
+          position: 'fixed',
+          right: 50,
+          bottom: 50
+        }}
+      >
+        <AddOutlined sx={{fontSize: 30}}/>
+      </IconButton>
     </JournalLayout>
   )
 }
